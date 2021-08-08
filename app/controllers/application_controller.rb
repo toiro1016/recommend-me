8c36be9df2853eae98416cad50b7f5a1ba5907bb
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-08-08 15:11:53 +0900

    ユーザーコントローラー　バリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :first_name, :last_name, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :self_introduction])
   end
 end
