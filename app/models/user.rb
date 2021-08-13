172c54874cab594ee3b0a2edf9f11a7a17bd544e
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-08-13 15:39:48 +0900

    見た目の調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,29 +7,27 @@ class User < ApplicationRecord
   has_many :reactions
 
 
-         with_options presence: true do
-          validates :nickname
-          # with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
-          #   validates :first_name
-          #   validates :last_name
-          # end
-        # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-        # validates :password, format: { with: VALID_PASSWORD_REGEX }
-        end
-        
-        validates :self_introduction, length: { maximum: 500 }
-
-        mount_uploader :profile_image, ProfileImageUploader
-
-        def update_without_current_password(params, *options)
-
-          if params[:password].blank? && params[:password_confirmation].blank?
-            params.delete(:password)
-            params.delete(:password_confirmation)
-          end
-      
-          result = update_attributes(params, *options)
-          clean_up_passwords
-          result
-        end
-      end
+ with_options presence: true do
+    validates :nickname
+    # with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+    #   validates :first_name
+    #   validates :last_name
+    # end
+    # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    # validates :password, format: { with: VALID_PASSWORD_REGEX }
+  end
+  
+  validates :self_introduction, length: { maximum: 500 }
+  mount_uploader :profile_image, ProfileImageUploader
+  
+  def update_without_current_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+end
