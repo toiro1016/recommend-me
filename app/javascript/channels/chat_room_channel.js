36b6518ba96bef1522e35fe4614480da699b3aff
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-08-25 23:17:28 +0900

    チャットコメントの保存

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -13,14 +13,15 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
     return alert(data['chat_message']);
   },
 
-  speak: function(chat_message) {
-    return this.perform('speak', { chat_message: chat_message });
+  speak: function(chat_message, chat_room_id) {
+    return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
   }
 });
 
 if(/chat_rooms/.test(location.pathname)) {
   $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
     if (e.key === "Enter") {
+      const chat_room_id = $('textarea').data('chat_room_id')
       appChatRoom.speak(e.target.value);
       e.target.value = '';
       e.preventDefault();
